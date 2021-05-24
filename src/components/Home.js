9b9b4f508623a081995127e048dbf07ceef5e559
Jun Park <jun.park06@gmail.com>
2021-05-24 09:40:51 -0700

    bro pls

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React, { useContext } from 'react';
-import Typography from '@material-ui/core/Typography'
-import { UserContext } from '../contexts/user-context'
-import { PhotosContext } from '../contexts/photos-context'
-import ContextTest from './ContextTest';
-import Link from '@material-ui/core/Link';
-import { Link as RouterLink } from 'react-router-dom';
-
-=======
 
 import React, { useContext } from 'react';
 import Link from '@material-ui/core/Link';
@@ -17,7 +7,6 @@ import { Link as RouterLink } from 'react-router-dom';
 import { UserContext } from '../contexts/user-context'
 import { PhotosContext } from '../contexts/photos-context'
 import ContextTest from './ContextTest';
->>>>>>> ffc3a6f35642e9e7e6bed6024109eb50c47be675
 
 const Home = () => {
 
